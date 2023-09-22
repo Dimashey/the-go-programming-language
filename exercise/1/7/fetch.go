c891c0deeeab345fd3c7d6b3d8fce97a132ee01a
Dimashey <sheykodmitriyjob@gmail.com>
2023-09-23 01:15:36 +0300

    finish exercise 1.7 1.8 1.9

---
+++ exercise/1/7/fetch.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		bytes, err := io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+
+		if err != nil || bytes == 0 {
+			fmt.Fprintf(os.Stderr, "fetch: coping %v. copied %d byte(s)\n", err, bytes)
+			os.Exit(1)
+		}
+	}
+}
---
+++ exercise/1/8/fetch.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const protocol = "https://"
+
+func getURL(url string) string {
+	if strings.HasPrefix(url, protocol) {
+		return url
+	}
+
+	return protocol + url
+}
+
+func main() {
+	for _, url := range os.Args[1:] {
+		u := getURL(url)
+
+		resp, err := http.Get(u)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		bytes, err := io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+
+		if err != nil || bytes == 0 {
+			fmt.Fprintf(os.Stderr, "fetch: coping %v. copied %d byte(s)\n", err, bytes)
+			os.Exit(1)
+		}
+	}
+}
